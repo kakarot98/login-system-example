8cab504cc440e156e6dfdad6564a0f06353e26e6
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2022-02-09 16:15:55 +0530

    created session saved as cookies

--- server/index.js
+++ server/index.js
@@ -10,7 +10,26 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    key: "userId",
+    secret: "subscribe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
 
 const db = mysql.createConnection({
   user: "root",
@@ -46,6 +65,15 @@ app.post("/register", (req, res) => {
   
 });
 
+
+app.get("/login", (req, res)=> {
+  if(req.session.user){
+    res.send({loggedIn: true, user: req.session.user})
+  } else {
+    res.send({loggedIn: false})
+  }
+})
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -62,6 +90,8 @@ app.post("/login", (req, res) => {
         //res.send(result);
         bcrypt.compare(password, result[0].passwords, (err, response)=>{
           if(response){
+            req.session.user = result
+            console.log(req.session.user)
             res.send(result)
           } else {
             res.send({ message: "wrong combination of username/password", err: "wrong combination" })
