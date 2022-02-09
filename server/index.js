dbf621759ae4a9d5f6dfabde1eddf62dbba1a2d5
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2022-02-09 15:27:30 +0530

    updated authentication

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 app.use(express.json());
@@ -14,37 +20,56 @@ const db = mysql.createConnection({
 });
 
 app.post("/register", (req, res) => {
-  const username = req.body.userName;
+  const username = req.body.username;
   const password = req.body.password;
-  db.query(
-    "INSERT INTO users (username, passwords) VALUES (?,?)",
-    [username, password],
-    (err, result) => {
-      if (err) {
-        res.send({ err: err });
-      } else {
-        res.send({ message: "user has been added" });
-      }
+  
+  bcrypt.hash(password, saltRounds, (err, hash)=> {
+
+    if(err){
+      console.log(err)
+      res.send({err:err})
     }
-  );
+    db.query(
+      "INSERT INTO users (username, passwords) VALUES (?,?)",
+      [username, hash],
+      (err, result) => {
+        if (err) {
+          res.send({ err: err });
+        } else {
+          res.send({ message: "user has been added" });
+        }
+      }
+    );
+  })
+
+
+  
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.userName;
+  const username = req.body.username;
   const password = req.body.password;
 
   db.query(
-    "SELECT * FROM users WHERE username = ? AND passwords = ?",
-    [username, password],
+    "SELECT * FROM users WHERE username = ?",
+    username,
     (err, result) => {
       if (err) {
-        console.log({ err: err });
+        res.send({ err: err });
       }
 
-      if (result) {
-        res.send(result);
+      if (result.length>0) {
+        //res.send(result);
+        bcrypt.compare(password, result[0].passwords, (err, response)=>{
+          if(response){
+            res.send(result)
+          } else {
+            res.send({ message: "wrong combination of username/password", err: "wrong combination" })
+          }
+        })
       } else {
-        res.send({ message: "wrong combination of username/password" });
+        console.log("no such thing found")
+        res.send({ message: "user does not exist", err: "404 not found" });
       }
     }
   );
